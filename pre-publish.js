f46a415a543c9f576e9bfef93fde55b3bcee606e
Bernhard Grünewaldt <github@gruenewaldt.net>
2015-11-30 01:01:03 +0100

    better css so that the icons fit with the text baseline.

--- pre-publish.js
+++ pre-publish.js
@@ -6,10 +6,3 @@ fse.copy('./node_modules/emojify.js/dist/images/basic', './assets/emojis', funct
     }
     console.log("success!");
 });
-
-fse.copy('./node_modules/emojify.js/dist/css/basic/emojify.css', './assets/emojify.css', function (err) {
-    if (err) {
-        return console.error(err);
-    }
-    console.log("success!");
-});
